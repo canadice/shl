761bd407e5591689f63bceb8c7a89f513b201d3c
canadice <33020567+canadice@users.noreply.github.com>
2021-05-05 08:17:30 +0200

    Adding scripts for specific API calls.

---
+++ scripts/analysis/playerProgress.R
@@ -0,0 +1,16 @@
+
+#################################################################
+##                       Player Progress                       ##
+##                                                             ##
+##                     Created: 2021-05-02                     ##
+##                    Last edit: 2021-05-02                    ##
+#################################################################
+
+### Loading API functions and loads relevant data sets
+source("scripts/API/apiSetup.R")
+
+players <- indStatsLoader(0)$players
+
+# save(players, file = "C:/Users/Canadice/Downloads/SHN Stats.RData")
+
+
