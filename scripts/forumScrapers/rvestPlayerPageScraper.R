eb478b7fe7a742f16c1508021c10319a6a305dd6
canadice <isak.hietala@liu.se>
2021-06-08 21:53:49 +0200

    Minor change

--- scripts/forumScrapers/rvestPlayerPageScraper.R
+++ scripts/forumScrapers/rvestPlayerPageScraper.R
@@ -100,7 +100,7 @@ playerScraper <-
         "Chicago Syndicate|Team |Detroit Falcons|Awards Committee|Manhattan Rage|",
         "SMJHL HO|Edmonton Blizzard|Los Angeles Panthers|Baltimore Platoon|Site |All-Star Committee|",
         "HO|Anchorage Armada|Winnipeg Aurora|Tampa Bay Barracuda|Czech Republic|PGS Grader|",
-        "Germany|Commissioner|Awards Committee|Management|IIHF|Historian|HOF Committee|"
+        "Germany|Commissioner|Awards Committee|Management|IIHF|Historian|HOF Committee"
       )
     
     USER <- 
